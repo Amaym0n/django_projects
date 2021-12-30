e1eaba13b5bd3dbb43ad337ef4a3b6db8b70708e
amaym0n <loco.skitls@gmail.com>
2021-12-30 15:40:03 +0300

    Added admin, meta class and verbose names for the models

--- test/thefirst/thefirst/apps/articles/models.py
+++ test/thefirst/thefirst/apps/articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
@@ -7,6 +8,16 @@ class Article(models.Model):
     article_author = models.CharField('Автор статьи', max_length=50)
     article_text = models.TextField('Текст статьи')
     pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
 
 
 class Comment(models.Model):
@@ -16,3 +27,10 @@ class Comment(models.Model):
 
     author_name = models.CharField('Автор комментария', max_length=50)
     comment_text = models.CharField('Текст комментария', max_length=300)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
