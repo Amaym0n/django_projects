e9bf296b8edda9dcbc8701aabf549b5f6d13ebf3
amaym0n <loco.skitls@gmail.com>
2021-12-30 14:43:27 +0300

    Added urls.py for the local app article

--- test/thefirst/thefirst/settings.py
+++ test/thefirst/thefirst/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os.path import join
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = Path(__file__).resolve().parent
+sys.path.append(str(join(PROJECT_DIR, 'apps')))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
