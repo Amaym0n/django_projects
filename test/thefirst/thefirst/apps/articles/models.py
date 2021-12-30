2dff651b88811716707c2cbac2250fba9e6d5532
amaym0n <loco.skitls@gmail.com>
2021-12-30 15:05:18 +0300

    Small fixes and update readme

--- test/thefirst/thefirst/apps/articles/models.py
+++ test/thefirst/thefirst/apps/articles/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    article_title = models.CharField('Название статьи', max_Length=200)
-    article_author = models.CharField('Автор статьи', max_Length=50)
+    article_title = models.CharField('Название статьи', max_length=200)
+    article_author = models.CharField('Автор статьи', max_length=50)
     article_text = models.TextField('Текст статьи')
     pub_date = models.DateTimeField()
 
@@ -14,5 +14,5 @@ class Comment(models.Model):
     # связывает комментарии со статьями. Параметр on_delete удаляет все комментарии для статьи, в случае ее удаления
     article = models.ForeignKey(Article, on_delete = models.CASCADE)
 
-    author_name = models.CharField('Автор комментария', max_Length=50)
-    comment_text = models.CharField('Текст комментария', max_Length=300)
+    author_name = models.CharField('Автор комментария', max_length=50)
+    comment_text = models.CharField('Текст комментария', max_length=300)
