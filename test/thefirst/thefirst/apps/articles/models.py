cc44b69bf968f1b399203335d5fa607929b1fa45
amaym0n <loco.skitls@gmail.com>
2021-12-30 15:00:14 +0300

    Updated models

--- test/thefirst/thefirst/apps/articles/models.py
+++ test/thefirst/thefirst/apps/articles/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    ...
+    article_title = models.CharField('Название статьи', max_Length=200)
+    article_author = models.CharField('Автор статьи', max_Length=50)
+    article_text = models.TextField('Текст статьи')
+    pub_date = models.DateTimeField()
+
 
 class Comment(models.Model):
-    ...
+
+    # связывает комментарии со статьями. Параметр on_delete удаляет все комментарии для статьи, в случае ее удаления
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+
+    author_name = models.CharField('Автор комментария', max_Length=50)
+    comment_text = models.CharField('Текст комментария', max_Length=300)
