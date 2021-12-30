e7f8639cf8a55d3f96ff5157fba83e0e77afb588
amaym0n <loco.skitls@gmail.com>
2021-12-30 17:34:26 +0300

    The last step for the compliting test project

--- test/thefirst/thefirst/apps/articles/urls.py
+++ test/thefirst/thefirst/apps/articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.test, name='test')
+    path('', views.main, name='main'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:article_id>/leave_comment/', views.leave_comment, name='leave_comment'),
 ]
